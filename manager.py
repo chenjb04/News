8cf25419eb360d30600310ccf2f78dffd6b09e81
w2580473897 <2580473897@qq.com>
2018-09-19 18:29:01 +0800

    项目的配置和数据库的配置

--- manager.py
+++ manager.py
@@ -3,6 +3,8 @@ __author__ = 'ChenJiaBao'
 __date__ = '2018/9/19 17:56'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
+from flask_wtf import CSRFProtect
 
 
 class Config(object):
@@ -11,13 +13,24 @@ class Config(object):
     # mysql配置
     SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/news'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
+
 # 加载配置
 app.config.from_object(Config)
+
 # 初始化数据库
 db = SQLAlchemy(app)
+
+# 初始化redis存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 开启csrf
+CSRFProtect(app)
 
 
 @app.route('/')
