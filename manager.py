e7612d1fd9c81dc564d8f0450574bf3d087dd3c7
w2580473897 <2580473897@qq.com>
2018-09-19 18:18:18 +0800

    项目的配置和数据库的配置

--- manager.py
+++ manager.py
@@ -2,8 +2,22 @@
 __author__ = 'ChenJiaBao'
 __date__ = '2018/9/19 17:56'
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+    # mysql配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -12,4 +26,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
