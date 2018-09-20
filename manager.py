8bde9fc7302496a9dc3c818428621050c885b3b6
w2580473897 <2580473897@qq.com>
2018-09-20 13:41:12 +0800

    集成数据库迁移命令

--- manager.py
+++ manager.py
@@ -7,6 +7,7 @@ from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -46,6 +47,11 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+
+# 将app和db关连
+Migrate(app, db)
+# 将数据库迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
