d1d5cf2cba8e8d2414d856f924f60bde131f5603
w2580473897 <2580473897@qq.com>
2018-10-22 12:53:08 +0800

    加载网站小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,9 +2,15 @@
 __author__ = 'ChenJiaBao'
 __date__ = '2018/9/20 15:02'
 from . import index_blu
-from flask import render_template
+from flask import render_template, current_app
 
 
 @index_blu.route('/')
 def index():
     return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    """加载网站小图标"""
+    return current_app.send_static_file('news/favicon.ico')
